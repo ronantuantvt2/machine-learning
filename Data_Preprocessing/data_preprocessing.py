a1b570a65a3c50c0b79c1aee01bdea1a03e91e11
Tuan Nguyen <ronantuantvt@gmail.com>
2018-01-14 22:47:45 -0500

    handle missing data

--- Data_Preprocessing/data_preprocessing.py
+++ Data_Preprocessing/data_preprocessing.py
@@ -5,10 +5,31 @@ import pandas as pd
 
 # Import dataset
 dataset = pd.read_csv('Data.csv')
-#print dataset
+# print dataset
 
 # Create metric of feature
 X = dataset.iloc[:, :-1].values
 Y = dataset.iloc[:, 3].values
+# print X
+# print Y
+
+# Taking care of missing data
+# Using imputer library of sklearn to handle this issue
+# Doc: http://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.Imputer.html
+from sklearn.preprocessing import Imputer
+# Determine which one is missing
+missingValues = 'NaN' 
+# How process missing data
+# mean : using average along the axis
+# median: 
+# most_frequent: using the most frequent value along the axis
+strategy = 'mean'
+# axis column: 0, row: 1 , default: 0
+axis = 0
+imputer = Imputer(missingValues, strategy, axis)
+# fit imputer object to dataset
+# take all column index 1,2
+imputer = imputer.fit(X[:, 1:3])
+# Replace data by the mean of column
+X[:, 1:3] = imputer.transform(X[:, 1:3])
 print X
-print Y
